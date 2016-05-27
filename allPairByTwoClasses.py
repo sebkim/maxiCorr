5b5d9759ac3889b216580033aaf9726ca55879d7
Sebo Kim <sanari85@gmail.com>
2016-05-27 18:06:10 -0400

    trivial 2.

--- allPairByTwoClasses.py
+++ allPairByTwoClasses.py
@@ -1,11 +1,13 @@
 # find all pair correlations by two classes (sick and normal).
 # x-axis is sick, and y-axis is normal.
+# it outputs sickAndNormalCorrFlattened.pickle and genePairNames.pickle, genePairIndnum.pickle .
 
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
 genes=pickle.load(open('./prePickles/gExpressions2.pickle','r'))
+geneNames=pickle.load(open('./prePickles/gNames2.pickle','r'))
 
 sickAndNormal=np.hsplit(genes,2)
 sick=sickAndNormal[0]
@@ -22,11 +24,22 @@ normalCorr=np.corrcoef(normal)
 numGene=sickCorr.shape[0]
 sickCorrFlattened=[]
 normalCorrFlattened=[]
+genePairNames=[]
+genePairIndnum=[]
 for rawI in range(numGene):
 	for rawJ in range(rawI+1,numGene):
 		sickCorrFlattened.append(sickCorr[rawI][rawJ])
 		normalCorrFlattened.append(normalCorr[rawI][rawJ])
+		genePairNames.append((geneNames[rawI],geneNames[rawJ]))
+		genePairIndnum.append((rawI,rawJ))
 
 pickle.dump(zip(sickCorrFlattened,normalCorrFlattened),open('./prePickles/sickAndNormalCorrFlattened.pickle','w'))
+pickle.dump(genePairNames,open('./prePickles/genePairNames.pickle','w'))
+pickle.dump(genePairIndnum,open('./prePickles/genePairIndnum.pickle','w'))
+
 # plt.scatter(sickCorrFlattened,normalCorrFlattened)
 # plt.show()
+
+# a=pickle.load(open('./prePickles/sickAndNormalCorrFlattened.pickle','r'))
+# b=pickle.load(open('./prePickles/genePairNames.pickle','r'))
+# c=pickle.load(open('./prePickles/genePairIndnum.pickle','r'))
